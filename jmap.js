cf12a4676da3c1c291d0ca26ca9c643563650c1c
tsukimaruhobby-create <tsukimaru.hobby@gmail.com>
2026-01-16 03:51:10 +0900

    Update jmap.js

--- jmap.js
+++ jmap.js
@@ -1,32 +1,9 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>月丸マップ</title>
-
-<link rel="stylesheet"
-  href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="./jmap.js"></script>
-
-<style>
-#jmap {
-  width: 100%;
-  height: 500px;
-}
-</style>
-
-<script>
-$(function(){
-  $('#jmap').jmap();
+var d = new jpmap.japanMap(document.getElementById("my-map"), {
+  showsPrefectureName: true,
+  width: 1000,
+  movesIslands: true,
+  lang: 'en',
+  onSelect: function(data){
+    alert(data.name);
+  }
 });
-</script>
-</head>
-
-<body>
-<h2 style="text-align:center">月丸マップ</h2>
-<div id="jmap"></div>
-</body>
-</html>
