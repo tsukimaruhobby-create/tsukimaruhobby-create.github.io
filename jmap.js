588a4ae907b6a1684fae44a4f22da71bf8358266
tsukimaruhobby-create <tsukimaru.hobby@gmail.com>
2026-01-16 03:33:14 +0900

    Update jmap.js

--- jmap.js
+++ jmap.js
@@ -1,14 +1,4 @@
-
-<html>
-<head>
-    <title>Jmap jQuery Example</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="./jmap.js"></script>
-    <script type = "text/javascript">
-        // ここに「Javascript」に記載する内容
-	$(document).ready(function() {
+$(document).ready(function() {
     $('#jmap').jmap({
         height: '450px',
         lineColor: '#bfbfbf',
@@ -89,35 +79,3 @@
     });
 });
                             
-    </script>
-</head>
-<body>
-    <!-- Jmap表示位置 -->
-    <div id="jmap">
-        <div class="jmap-infobox">
-            <h5 class="h5">世界に一つだけの地図</h5>
-            <p>地図をクリックするとモーダルが表示されます。</p>
-        </div>
-    </div>
-
-    <!-- モーダル用HTML -->
-    <div class="modal fade" id="prefectureModal" tabindex="-1" role="dialog" aria-labelledby="prefectureModalCenterTitle" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="prefectureModalTitle"></h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body overflow-auto" style="height:350px;">
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</body>
-</html>
-            
