0b59ab0eddd85161b9073428afdf9b7eff88ff9a
tsukimaruhobby-create <tsukimaru.hobby@gmail.com>
2026-01-16 03:08:38 +0900

    Update jmap.js

--- jmap.js
+++ jmap.js
@@ -1,83 +1,123 @@
+
+<html>
 <head>
-
-    省略    
-
-    <script type="text/javascript" src="https://unpkg.com/japan-map-js@1.0.1/dist/jpmap.min.js"></script>　//追記
-    <script type="text/javascript" src="dist/jpmap.min.js"></script>　//追記
-</head>
-<div id="my-map"></div>
-<script>
-<script>
-var areaLinks = {
-		         1:"https://www.pref.hokkaido.lg.jp/",　//リンク先URL
-                 2:"https://www.pref.aomori.lg.jp/",
-　　　　　　　　　 3:"https://www.pref.iwate.lg.jp/",
-                 4:"https://www.pref.miyagi.lg.jp/",
-                            ・
-                            ・
-                        　　省略
-                            ・
-                            ・
-	    };
-
-var d = new jpmap.japanMap(document.getElementById("my-map"), {
-    areas: [
-    {code : 1, name: "北海道", color: "#7f7eda", hoverColor: "#b3b2ee"},
-    {code : 2, name: "青森", color: "#759ef4", hoverColor: "#98b9ff"},
-    {code : 3, name: "岩手", color: "#759ef4", hoverColor: "#98b9ff"},
-    {code : 4, name: "宮城", color: "#759ef4", hoverColor: "#98b9ff"},
-    {code : 5, name: "秋田", color: "#759ef4", hoverColor: "#98b9ff"},
-    {code : 6, name: "山形", color: "#759ef4", hoverColor: "#98b9ff"},
-    {code : 7, name: "福島", color: "#759ef4", hoverColor: "#98b9ff"},
-    {code : 8, name: "茨城", color: "#7ecfea", hoverColor: "#b7e5f4"},
-    {code : 9, name: "栃木", color: "#7ecfea", hoverColor: "#b7e5f4"},
-    {code : 10, name: "群馬", color: "#7ecfea", hoverColor: "#b7e5f4"},
-    {code : 11, name: "埼玉", color: "#7ecfea", hoverColor: "#b7e5f4"},
-    {code : 12, name: "千葉", color: "#7ecfea", hoverColor: "#b7e5f4"},
-    {code : 13, name: "東京", color: "#7ecfea", hoverColor: "#b7e5f4"},
-    {code : 14, name: "神奈川", color: "#7ecfea", hoverColor: "#b7e5f4"},
-    {code : 15, name: "新潟", color: "#7cdc92", hoverColor: "#aceebb"},
-    {code : 16, name: "富山", color: "#7cdc92", hoverColor: "#aceebb"},
-    {code : 17, name: "石川", color: "#7cdc92", hoverColor: "#aceebb"},
-    {code : 18, name: "福井", color: "#7cdc92", hoverColor: "#aceebb"},
-    {code : 19, name: "山梨", color: "#7cdc92", hoverColor: "#aceebb"},
-    {code : 20, name: "長野", color: "#7cdc92", hoverColor: "#aceebb"},
-    {code : 21, name: "岐阜", color: "#7cdc92", hoverColor: "#aceebb"},
-    {code : 22, name: "静岡", color: "#7cdc92", hoverColor: "#aceebb"},
-    {code : 23, name: "愛知", color: "#7cdc92", hoverColor: "#aceebb"},
-    {code : 24, name: "三重", color: "#ffe966", hoverColor: "#fff19c"},
-    {code : 25, name: "滋賀", color: "#ffe966", hoverColor: "#fff19c"},
-    {code : 26, name: "京都", color: "#ffe966", hoverColor: "#fff19c"},
-    {code : 27, name: "大阪", color: "#ffe966", hoverColor: "#fff19c"},
-    {code : 28, name: "兵庫", color: "#ffe966", hoverColor: "#fff19c"},
-    {code : 29, name: "奈良", color: "#ffe966", hoverColor: "#fff19c"},
-    {code : 30, name: "和歌山", color: "#ffe966", hoverColor: "#fff19c"},
-    {code : 31, name: "鳥取", color: "#ffcc66", hoverColor: "#ffe0a3"},
-    {code : 32, name: "島根", color: "#ffcc66", hoverColor: "#ffe0a3"},
-    {code : 33, name: "岡山", color: "#ffcc66", hoverColor: "#ffe0a3"},
-    {code : 34, name: "広島", color: "#ffcc66", hoverColor: "#ffe0a3"},
-    {code : 35, name: "山口", color: "#ffcc66", hoverColor: "#ffe0a3"},
-    {code : 36, name: "徳島", color: "#fb9466", hoverColor: "#ffbb9c"},
-    {code : 37, name: "香川", color: "#fb9466", hoverColor: "#ffbb9c"},
-    {code : 38, name: "愛媛", color: "#fb9466", hoverColor: "#ffbb9c"},
-    {code : 39, name: "高知", color: "#fb9466", hoverColor: "#ffbb9c"},
-    {code : 40, name: "福岡", color: "#ff9999", hoverColor: "#ffbdbd"},
-    {code : 41, name: "佐賀", color: "#ff9999", hoverColor: "#ffbdbd"},
-    {code : 42, name: "長崎", color: "#ff9999", hoverColor: "#ffbdbd"},
-    {code : 43, name: "熊本", color: "#ff9999", hoverColor: "#ffbdbd"},
-    {code : 44, name: "大分", color: "#ff9999", hoverColor: "#ffbdbd"},
-    {code : 45, name: "宮崎", color: "#ff9999", hoverColor: "#ffbdbd"},
-    {code : 46, name: "鹿児島", color: "#ff9999", hoverColor: "#ffbdbd"},
-    {code : 47, name: "沖縄", color: "#eb98ff", hoverColor: "#f5c9ff"},
-  ],
-
-  showsPrefectureName: true,
-  width: 1000,
-  movesIslands: true,
-  borderLineColor: "#000000",
-  lang: 'ja',
-  onSelect: function(data){
-     location.href = areaLinks[data.area.code];
-  }
+    <title>Jmap jQuery Example</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="./jmap.js"></script>
+    <script type = "text/javascript">
+        // ここに「Javascript」に記載する内容
+	$(document).ready(function() {
+    $('#jmap').jmap({
+        height: '450px',
+        lineColor: '#bfbfbf',
+        lineWidth: 1,
+        showInfobox: true,
+        backgroundRadius: '0.3rem',
+        backgroundPadding: '1rem',
+        backgroundColor: '#ff000000',
+        prefectureClass: 'prefecture',
+        prefectureLineColor: '#ffffff',
+        prefectureLineWidth: 1,
+        prefectureLineHoverColor : '#fff',
+        fontSize: '0.8rem',
+        fontColor: '#000',
+        font: 'serif',
+        areas: [
+        {code : 1, name: "北海道", color: "#7f7eda", hoverColor: "#b3b2ee" },
+        {code : 2, name: "青森", color: "#759ef4", hoverColor: "#98b9ff" },
+        {code : 3, name: "岩手", color: "#759ef4", hoverColor: "#98b9ff" },
+        {code : 4, name: "宮城", color: "#759ef4", hoverColor: "#98b9ff" },
+        {code : 5, name: "秋田", color: "#759ef4", hoverColor: "#98b9ff" },
+        {code : 6, name: "山形", color: "#759ef4", hoverColor: "#98b9ff" },
+        {code : 7, name: "福島", color: "#759ef4", hoverColor: "#98b9ff" },
+        {code : 8, name: "茨城", color: "#7ecfea", hoverColor: "#b7e5f4" },
+        {code : 9, name: "栃木", color: "#7ecfea", hoverColor: "#b7e5f4" },
+        {code : 10, name: "群馬", color: "#7ecfea", hoverColor: "#b7e5f4" },
+        {code : 11, name: "埼玉", color: "#7ecfea", hoverColor: "#b7e5f4" },
+        {code : 12, name: "千葉", color: "#7ecfea", hoverColor: "#b7e5f4" },
+        {code : 13, name: "東京", color: "#7ecfea", hoverColor: "#b7e5f4" },
+        {code : 14, name: "神奈川", color: "#7ecfea", hoverColor: "#b7e5f4" },
+        {code : 15, name: "新潟", color: "#7cdc92", hoverColor: "#aceebb" },
+        {code : 16, name: "富山", color: "#7cdc92", hoverColor: "#aceebb" },
+        {code : 17, name: "石川", color: "#7cdc92", hoverColor: "#aceebb" },
+        {code : 18, name: "福井", color: "#7cdc92", hoverColor: "#aceebb" },
+        {code : 19, name: "山梨", color: "#7cdc92", hoverColor: "#aceebb" },
+        {code : 20, name: "長野", color: "#7cdc92", hoverColor: "#aceebb" },
+        {code : 21, name: "岐阜", color: "#7cdc92", hoverColor: "#aceebb" },
+        {code : 22, name: "静岡", color: "#7cdc92", hoverColor: "#aceebb" },
+        {code : 23, name: "愛知", color: "#7cdc92", hoverColor: "#aceebb" },
+        {code : 24, name: "三重", color: "#ffe966", hoverColor: "#fff19c" },
+        {code : 25, name: "滋賀", color: "#ffe966", hoverColor: "#fff19c" },
+        {code : 26, name: "京都", color: "#ffe966", hoverColor: "#fff19c" },
+        {code : 27, name: "大阪", color: "#ffe966", hoverColor: "#fff19c" },
+        {code : 28, name: "兵庫", color: "#ffe966", hoverColor: "#fff19c" },
+        {code : 29, name: "奈良", color: "#ffe966", hoverColor: "#fff19c" },
+        {code : 30, name: "和歌山", color: "#ffe966", hoverColor: "#fff19c" },
+        {code : 31, name: "鳥取", color: "#ffcc66", hoverColor: "#ffe0a3" },
+        {code : 32, name: "島根", color: "#ffcc66", hoverColor: "#ffe0a3" },
+        {code : 33, name: "岡山", color: "#ffcc66", hoverColor: "#ffe0a3" },
+        {code : 34, name: "広島", color: "#ffcc66", hoverColor: "#ffe0a3" },
+        {code : 35, name: "山口", color: "#ffcc66", hoverColor: "#ffe0a3" },
+        {code : 36, name: "徳島", color: "#fb9466", hoverColor: "#ffbb9c" },
+        {code : 37, name: "香川", color: "#fb9466", hoverColor: "#ffbb9c" },
+        {code : 38, name: "愛媛", color: "#fb9466", hoverColor: "#ffbb9c" },
+        {code : 39, name: "高知", color: "#fb9466", hoverColor: "#ffbb9c" },
+        {code : 40, name: "福岡", color: "#ff9999", hoverColor: "#ffbdbd" },
+        {code : 41, name: "佐賀", color: "#ff9999", hoverColor: "#ffbdbd" },
+        {code : 42, name: "長崎", color: "#ff9999", hoverColor: "#ffbdbd" },
+        {code : 43, name: "熊本", color: "#ff9999", hoverColor: "#ffbdbd" },
+        {code : 44, name: "大分", color: "#ff9999", hoverColor: "#ffbdbd" },
+        {code : 45, name: "宮崎", color: "#ff9999", hoverColor: "#ffbdbd" },
+        {code : 46, name: "鹿児島", color: "#ff9999", hoverColor: "#ffbdbd" },
+        {code : 47, name: "沖縄", color: "#eb98ff", hoverColor: "#f5c9ff" }
+        ],
+        onLoad: function(e, data) {
+            $(this).html('<strong>' + data.name + '</strong>');
+        },
+        onSelect: function(e, data) {
+            $('#prefectureModal').find('#prefectureModalTitle')
+            .html(data.area8.name + " - " + data.area11.name + " - " + data.name + data.full)
+            .end().find('.modal-body')
+            .html(JSON.stringify(data, null, 4))
+            .end().modal('show');
+        },
+        onHover: function(e, data) {
+            console.log(data);
+        }
+    });
 });
-</script>
+                            
+    </script>
+</head>
+<body>
+    <!-- Jmap表示位置 -->
+    <div id="jmap"
+        <div class="jmap-infobox">
+            <h5 class="h5">世界に一つだけの地図</h5>
+            <p>地図をクリックするとモーダルが表示されます。</p>
+        </div>
+    ></div>
+
+    <!-- モーダル用HTML -->
+    <div class="modal fade" id="prefectureModal" tabindex="-1" role="dialog" aria-labelledby="prefectureModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="prefectureModalTitle"></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body overflow-auto" style="height:350px;">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+            
