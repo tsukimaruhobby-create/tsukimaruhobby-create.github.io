1331826b8f9e3384d35bee8d3f737758bc816e0d
tsukimaruhobby-create <tsukimaru.hobby@gmail.com>
2026-01-16 23:41:07 +0900

    Update jmap.js

--- jmap.js
+++ jmap.js
@@ -1,3 +1,6 @@
+<script type="text/javascript" src="dist/jpmap.min.js"></script>
+<script type="text/javascript" src="https://unpkg.com/japan-map-js@1.0.1/dist/jpmap.min.js"></script>
+<div id="my-map"></div>
 var d = new jpmap.japanMap(document.getElementById("my-map"), {
   showsPrefectureName: true,
   width: 1000,
